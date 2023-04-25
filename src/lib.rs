17ccbe4254a8271e6c1525035d6c69fa551397c9
Ravi Nagabhyru <ravi.nagabhyru@gmail.com>
2023-04-25 16:46:16 -0400

    added splits and general security information

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,56 @@ pub struct Dividend {
     pub value: f64,
 }
 
+#[derive(Deserialize, Debug)]
+pub struct Split {
+    pub date: String,
+    pub split: String,
+}
+
+#[allow(dead_code)]
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct AssetInformation {
+    code: String,
+    #[serde(rename = "Type")]
+    asset_type: String,
+    name: String,
+    exchange: String,
+    currency_code: String,
+    currency_name: String,
+    currency_symbol: String,
+    country_name: String,
+    #[serde(rename = "CountryISO")]
+    country_iso: String,
+    #[serde(rename = "ISIN")]
+    isin: String,
+    #[serde(rename = "LEI")]
+    lei: String,
+    primary_ticker: String,
+    #[serde(rename = "CUSIP")]
+    cusip: String,
+    #[serde(rename = "CIK")]
+    cik: String,
+    #[serde(rename = "IPODate")]
+    ipo_date: String,
+    sector: String,
+    industry: String,
+    gic_sector: String,
+    gic_group: String,
+    gic_industry: String,
+    gic_sub_industry: String,
+    home_category: String,
+    is_delisted: bool,
+}
+
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct FundamentalsResponse {
+    general: AssetInformation,
+}
+
+
 #[derive(Error, Debug)]
 pub enum EodHistDataError {
     #[error("fetching the data from eodhistoricaldata failed with status code {0}")]
@@ -118,7 +168,7 @@ impl EodHistConnector {
         Ok(quotes)
     }
 
-    /// Retrieve the quote history for the given ticker form date start to end (inklusive), if available
+    /// Retrieve the dividend history for the given ticker form date start to end (inclusive), if available
     pub async fn get_dividend_history(
         &self,
         ticker: &str,
@@ -134,6 +184,40 @@ impl EodHistConnector {
         let resp = self.send_request(&url).await?;
         let dividends: Vec<Dividend> = serde_json::from_value(resp)?;
         Ok(dividends)
+    }
+
+    /// Retrieve the split history for the given ticker form date start to end (inclusive), if available
+    pub async fn get_split_history(
+        &self,
+        ticker: &str,
+        start: NaiveDate,
+    ) -> Result<Vec<Split>, EodHistDataError> {
+        let url: String = format!(
+            "{}/splits/{}?from={}&api_token={}&fmt=json",
+            self.url,
+            ticker,
+            start.format("%Y-%m-%d"),
+            self.api_token
+        );
+        let resp = self.send_request(&url).await?;
+        let splits: Vec<Split> = serde_json::from_value(resp)?;
+        Ok(splits)
+    }
+
+    /// Retrieve the fundamentals for the given ticker
+    pub async fn get_asset_information(
+        &self,
+        ticker: &str,
+    ) -> Result<AssetInformation, EodHistDataError> {
+        let url: String = format!(
+            "{}/fundamentals/{}?api_token={}",
+            self.url,
+            ticker,
+            self.api_token
+        );
+        let resp = self.send_request(&url).await?;
+        let fundamentals: FundamentalsResponse = serde_json::from_value(resp)?;
+        Ok(fundamentals.general)
     }
 
     /// Send request to eodhistoricaldata server and transform response to JSON value
@@ -186,5 +270,49 @@ mod tests {
             tokio_test::block_on(provider.get_dividend_history("AAPL.US", start)).unwrap();
 
         assert!(dividends.len() >= 4);
+    }
+
+    #[test]
+    fn test_get_split_history() {
+        // Use the official test token
+        let token = "OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX".to_string();
+        let provider = EodHistConnector::new(token);
+        let start = NaiveDate::from_ymd_opt(2020, 01, 01).unwrap();
+        let splits =
+            tokio_test::block_on(provider.get_split_history("AAPL.US", start)).unwrap();
+
+        assert!(splits.len() >= 1);
+    }
+
+    #[test]
+    fn test_get_asset_information() {
+        // Use the official test token
+        let token = "OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX".to_string();
+        let provider = EodHistConnector::new(token);
+        let info = tokio_test::block_on(provider.get_asset_information("AAPL.US")).unwrap();
+
+        assert_eq!(info.code, "AAPL");
+        assert_eq!(info.asset_type, "Common Stock");
+        assert!(info.name.contains("Apple Inc"));
+        assert_eq!(info.exchange, "NASDAQ");
+        assert_eq!(info.currency_code, "USD");
+        assert_eq!(info.currency_name, "US Dollar");
+        assert_eq!(info.currency_symbol, "$");
+        assert_eq!(info.country_name, "USA");
+        assert_eq!(info.country_iso, "US");
+        assert_eq!(info.isin, "US0378331005");
+        assert_eq!(info.lei, "HWUPKR0MPOU8FGXBT394");
+        assert_eq!(info.primary_ticker, "AAPL.US");
+        assert_eq!(info.cusip, "037833100");
+        assert_eq!(info.cik, "320193");
+        assert_eq!(info.ipo_date, "1980-12-12");
+        assert_eq!(info.sector, "Technology");
+        assert_eq!(info.industry, "Consumer Electronics");
+        assert_eq!(info.gic_sector, "Information Technology");
+        assert_eq!(info.gic_group, "Technology Hardware & Equipment");
+        assert_eq!(info.gic_industry, "Technology Hardware, Storage & Peripherals");
+        assert_eq!(info.gic_sub_industry, "Technology Hardware, Storage & Peripherals");
+        assert_eq!(info.home_category, "Domestic");
+        assert_eq!(info.is_delisted, false);
     }
 }
